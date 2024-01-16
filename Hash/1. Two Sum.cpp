68e63e677d87626df30b96de205f79fb35d0fdf1
00757129 <58520935+00757129@users.noreply.github.com>
2024-01-16 13:59:26 +0800

    Update 1. Two Sum.cpp

--- Hash/1. Two Sum.cpp
+++ Hash/1. Two Sum.cpp
@@ -1,33 +1,34 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-   int max=nums[0],min=nums[0],i;
-    for(i=1;i<numsSize;i++){
-        max=max>nums[i]?max:nums[i];
-        min=min<nums[i]?min:nums[i];
-    }
-    int arrsize=max-min+1;
-    int *HT=calloc(arrsize--,sizeof(int));
-    HT[nums[0]-min]=-1;
-    for(i=1;i<numsSize;i++){
-        HT[nums[i]-min]=i;
-    }
-    *returnSize=2;
-    int *ret=(int*) malloc(2*sizeof(int)),index2;
-    for(i=0;i<numsSize;i++){
-        index2=target-nums[i]-min;
-        if(index2<0||index2>arrsize)continue;
-        if(HT[index2]){
-            ret[0]=i;
-            ret[1]=HT[index2];
-            if(HT[index2]<0)ret[1]=0;
-            if(ret[0]==ret[1]){
-                continue;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // 宣告一個 Hash Table 來儲存目前查看過的數字。
+        // （以數字作為 Key、數字所在的 Index 作為 Value）
+        unordered_map<int, int> discovered;
+        
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            // 對於每個數字 nums[i] 來說，其要找到對應的數為 target - nums[i]。
+            // 例如，假設 num[i] = 2，其要找的數字就是 target - 2。
+            int num_to_find = target - nums[i];
+            
+            
+            if (discovered.find(num_to_find) == discovered.end()) {
+                
+                // 假使我們從 Hash Table 找不到 num_to_find，
+                // 那就先把目前的數字作為 Key、其 Index 作為 Value 暫存起來（未來該數可能是、也可能不是答案之一）。
+                discovered[nums[i]] = i;
+                
+            } else {
+                
+                // 反之，假使在儲存清單存在 num_to_find（意味該數曾經查看過），
+                // 代表我們已經找到答案了。
+                // 因此回傳 num_to_find 的 Index 及目前所在的 Index。
+                return vector<int>{discovered[num_to_find], i};
             }
-            free(HT);
-            return ret;
         }
+        
+        return {};
     }
-    return 0;
-}
+};

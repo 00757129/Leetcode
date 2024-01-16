7b92b5ba04b298377bab49cc32bfbd5f941aa918
00757129 <58520935+00757129@users.noreply.github.com>
2024-01-16 14:58:57 +0800

    Update 119. Pascal'sTriangleII_C++.cpp

--- DynamicProgramming/119. Pascal's Triangle II/119. Pascal'sTriangleII_C++.cpp
+++ DynamicProgramming/119. Pascal's Triangle II/119. Pascal'sTriangleII_C++.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> kRows(rowIndex+1);//第K行的vector大小为 rowIndex+1
-        for(int i = 0; i <= rowIndex; i++)//利用前一行求后一行，第K行要循环K遍
+        vector<int> kRows(rowIndex+1);
+        for(int i = 0; i <= rowIndex; i++)
             {
-            kRows[i] = 1;//行末尾为1
-            for(int j = i; j > 1; j--)//每一行的更新过程
+            kRows[i] = 1;
+            for(int j = i; j > 1; j--)
                 {
                     kRows[j-1] = kRows[j-2] + kRows[j-1];
                 }
@@ -13,6 +13,4 @@ public:
         return kRows;   
     }
 };
-————————————————
-版权声明：本文为CSDN博主「QuantumYou」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/QuantumYou/article/details/120057186
+

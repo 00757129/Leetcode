c03f09b0e674106304abddf5e82f5193a185f8ef
00757129 <58520935+00757129@users.noreply.github.com>
2024-03-03 15:22:49 +0800

    Update 88_Merge Sorted Array.c

--- Sort/88_Merge Sorted Array/88_Merge Sorted Array.c
+++ Sort/88_Merge Sorted Array/88_Merge Sorted Array.c
@@ -1,18 +1,26 @@
-public void merge(int[] nums1, int m, int[] nums2, int n) {
-		
-		while(m > 0 && n > 0){
-			if(nums1[m-1] > nums2[n-1]){
-				nums1[m+n-1] = nums1[m-1];
-				m--;
-			}
-			else{
-				nums1[m+n-1] = nums2[n-1];
-				n--;
-			}
-			
-		}
-		while(n>0){
-			nums1[m+n-1] = nums2[n-1];
-			n--;
-		}
-	}
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int *tmp = (int *)malloc(sizeof(int)*(m+n));
+    int nums1Index=0,nums2Index=0,tmpIndex = 0;
+
+    while(nums1Index<m && nums2Index<n){
+        if(nums1[nums1Index]<nums2[nums2Index]){
+            tmp[tmpIndex++] = nums1[nums1Index++];
+        }else{
+            tmp[tmpIndex++] = nums2[nums2Index++];
+        }
+    }
+    int i;
+    if(nums1Index==m){
+         for(i = nums2Index;i<n;i++)
+            tmp[tmpIndex++] = nums2[i];
+    }
+    else if(nums2Index == n){
+        for(i = nums1Index;i<m;i++)
+            tmp[tmpIndex++] = nums1[i];
+    }
+    for(i = 0;i<m+n;i++)
+        nums1[i] = tmp[i];
+
+    free(tmp);
+
+}
